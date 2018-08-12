e17d8d1c3cd8c249b485d32fe783bcccc9aefd11
Pramod Jacob <psjacob68@gmail.com>
2018-08-12 10:10:39 -0400

    Refactor create action for tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,49 +5,45 @@ class TagsController < ApplicationController
     tag = Tag.find_or_create_tag_for(current_user, tag_params)
              .attach_to_transaction_with_id(tag_params[:transaction_id])
 
-    if tag.valid? && tag_attached_to_transaction?(tag, params[:transaction_id])
+    if tag.valid? && attached_to_transaction?(tag, tag_params[:transaction_id])
       successful_create(tag)
     else
       failed_create(tag)
     end
   end
 
+  def destroy; end
+
   private
 
   def tag_params
-    params.permit(:name, :transaction_id)
+    params.permit(:id, :name, :transaction_id)
   end
 
-  def tag_attached_to_transaction?(tag, transaction_id)
+  def attached_to_transaction?(tag, transaction_id)
     return true unless params[:transaction_id]
     tag.attached_to_transaction?(transaction_id)
   end
 
   def successful_create(tag)
-    render json: {
-      message: 'Tag successfully saved',
-      content: display_content_for(tag)
-    }, status: 200
-  end
-
-  def failed_create(tag)
-    render json: {
-      message: 'Could not create tag',
-      content: display_errors_for(tag)
-    }, status: 400
-  end
-
-  def display_content_for(tag)
     tag_json = tag.jsonify
     if params[:transaction_id]
       tag_json[:transaction_id] = params[:transaction_id].to_i
     end
-    tag_json
+
+    render json: {
+      message: 'Tag successfully saved',
+      content: tag_json
+    }, status: 200
   end
 
-  def display_errors_for(tag)
+  def failed_create(tag)
     errors = tag.errors.full_messages
     errors << 'Could not find transaction item' if params[:transaction_id]
-    errors
+
+    render json: {
+      message: 'Could not create tag',
+      content: errors
+    }, status: 400
   end
 end
